3ff26af80cdcf93f895de0cff900d63c57bef47b
ShireenShamil <shireenshamil11@gmail.com>
2025-07-09 19:52:56 +0530

    update5

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -30,17 +30,23 @@ const Navbar = ({isDarkMode , setIsDarkMode}) => {
 
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+    {/* <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
         <Image src={assets.header_bg_color} alt='' className='w-full'/>
-    </div>
+    </div> */}
+
+    {!isDarkMode && (
+        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+            <Image src={assets.header_bg_color} alt='' className='w-full'/>
+        </div>
+    )}
    <nav
   className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
     flex items-center justify-between z-50
-    ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}
+    ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}
 >
 
         <a href='#top'>
-            <Image alt='' src={assets.logo} className='w-28 cursor-pointer mr-14' />
+            <Image alt='' src={ isDarkMode? assets.logo_dark :   assets.logo} className='w-28 cursor-pointer mr-14' />
         </a>
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : " bg-white shadow-sm bg-opacity-50"}`}>
             <li><a className='font-Ovo' href='#top'>Home</a></li>
