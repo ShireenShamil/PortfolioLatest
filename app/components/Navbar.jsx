ee9f36120f4c6f6e41bd545de4abc25f38238ff8
ShireenShamil <shireenshamil11@gmail.com>
2025-07-09 18:47:08 +0530

    update4

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import React, { useEffect, useRef, useState } from 'react';
 
 
-const Navbar = () => {
+const Navbar = ({isDarkMode , setIsDarkMode}) => {
 
     const sideMenuRef = useRef();
     const [isScroll , setIsScroll] = useState(false);
@@ -51,8 +51,8 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-4'>
-            <button>
-                <Image src={assets.moon_icon} alt='' className='w-6' />
+            <button onClick={() => setIsDarkMode(prev => !prev)}>
+                <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
             </button>
             <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image alt=""   src={assets.arrow_icon} className='w-3' /> </a>
             <button className='block md:hidden ml-3' onClick={openMenu}>
