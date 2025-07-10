a36269ac7eb680daf5ab93695b41d64f25982c14
ShireenShamil <shireenshamil11@gmail.com>
2025-07-10 10:15:53 +0530

    update9

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -2,7 +2,7 @@ import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Services = () => {
+const Services = ({isDarkMode}) => {
   return (
     <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
 
@@ -13,10 +13,10 @@ const Services = () => {
       
       <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
         {serviceData.map(({icon , title, description, link}, index) => (
-            <div className='border border-gray-400 rounded-lg px-8 py-12 hover:[box-shadow:4px_4px_0_#000]  hover:[background-color:#fcf4ff] cursor-pointer hover:-translate-y-1 duration-500' key={index}>
+            <div className='border border-gray-400 rounded-lg px-8 py-12 hover:[box-shadow:4px_4px_0_#000]  hover:[background-color:#fcf4ff] cursor-pointer hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white ' key={index}>
                 <Image src={icon} alt='icon' className='w-10' />
-                <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
-                <p className='text-sm text-gray-600 leading-5'>{description}</p>
+                <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
+                <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>{description}</p>
 
                 <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read More <Image alt='' src={assets.right_arrow} className='w-4'/></a>
             </div>
