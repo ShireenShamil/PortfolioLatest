7242f04a18d365c48f814ce224a06a1e59e6490d
ShireenShamil <shireenshamil11@gmail.com>
2025-07-10 11:25:29 +0530

    update 12

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,29 +1,52 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Services = ({isDarkMode}) => {
   return (
-    <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
+    <motion.div
+     initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+     id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
 
-         <h4 className='text-center mb-2 text-lg font-Ovo'>What I Offer</h4>
-      <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
+         <motion.h4 
+          initial={{opacity:0 , y:-20}}
+    whileInView={{opacity:1 , y:0}}
+    transition={{duration:0.5 , delay:0.3}}
+         className='text-center mb-2 text-lg font-Ovo'>What I Offer</motion.h4>
+      <motion.h2 
+       initial={{opacity:0 , y:-20}}
+    whileInView={{opacity:1 , y:0}}
+    transition={{duration:0.5 , delay:0.5}}
+      className='text-center text-5xl font-Ovo'>My Services</motion.h2>
 
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I offer full-stack web development services including responsive frontend design using React and Tailwind CSS, seamless UI/UX design with tools like Figma, and robust backend development using Node.js and Express. From crafting intuitive user experiences to building scalable APIs, I ensure clean, efficient, and performance-driven solutions. My goal is to deliver modern, user-focused web applications that meet both aesthetic and functional needs.</p>
+      <motion.p
+       initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.5 , delay:0.7}}
+       className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I offer full-stack web development services including responsive frontend design using React and Tailwind CSS, seamless UI/UX design with tools like Figma, and robust backend development using Node.js and Express. From crafting intuitive user experiences to building scalable APIs, I ensure clean, efficient, and performance-driven solutions. My goal is to deliver modern, user-focused web applications that meet both aesthetic and functional needs.</motion.p>
       
-      <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
+      <motion.div
+       initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.6 , delay:0.9}}
+       className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'>
         {serviceData.map(({icon , title, description, link}, index) => (
-            <div className='border border-gray-400 rounded-lg px-8 py-12 hover:[box-shadow:4px_4px_0_#000]  hover:[background-color:#fcf4ff] cursor-pointer hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white ' key={index}>
+            <motion.div
+            whileHover={{scale:1.05}}
+             className='border border-gray-400 rounded-lg px-8 py-12 hover:[box-shadow:4px_4px_0_#000]  hover:[background-color:#fcf4ff] cursor-pointer hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white ' key={index}>
                 <Image src={icon} alt='icon' className='w-10' />
                 <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                 <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>{description}</p>
 
                 <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read More <Image alt='' src={assets.right_arrow} className='w-4'/></a>
-            </div>
+            </motion.div>
         ))}
 
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,20 +1,44 @@
 import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Work = ({isDarkMode}) => {
   return (
-    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+    <motion.div
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+     id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
 
-         <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
-      <h2 className='text-center text-5xl font-Ovo'>My Latest Works</h2>
+         <motion.h4
+           initial={{opacity:0 , y:-20}}
+    whileInView={{opacity:1 , y:0}}
+    transition={{duration:0.5 , delay:0.3}}
+          className='text-center mb-2 text-lg font-Ovo'>My Portfolio</motion.h4>
+      <motion.h2
+      initial={{opacity:0 , y:-20}}
+    whileInView={{opacity:1 , y:0}}
+    transition={{duration:0.5 , delay:0.5}}
+       className='text-center text-5xl font-Ovo'>My Latest Works</motion.h2>
 
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I offer full-stack web development services including responsive frontend design using React and Tailwind CSS, seamless UI/UX design with tools like Figma, and robust backend development using Node.js and Express. From crafting intuitive user experiences to building scalable APIs, I ensure clean, efficient, and performance-driven solutions. My goal is to deliver modern, user-focused web applications that meet both aesthetic and functional needs.</p>
+      <motion.p 
+        initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.5 , delay:0.7}}
+      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I offer full-stack web development services including responsive frontend design using React and Tailwind CSS, seamless UI/UX design with tools like Figma, and robust backend development using Node.js and Express. From crafting intuitive user experiences to building scalable APIs, I ensure clean, efficient, and performance-driven solutions. My goal is to deliver modern, user-focused web applications that meet both aesthetic and functional needs.</motion.p>
       
 
-      <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 my-10 dark:text-black'>
+      <motion.div 
+      initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.6 , delay:0.9}}
+      className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 my-10 dark:text-black'>
               {workData.map((project, index) => (
-                  <div className='aspect-square bg-no-repeat bg-cover bg-center bg-lg relative cursor-pointer group' style={{backgroundImage: `url(${project.bgImage})`}} key={index}>
+                  <motion.div 
+                   whileHover={{scale:1.05}}
+                   transition={{duration:0.3}}
+                  className='aspect-square bg-no-repeat bg-cover bg-center bg-lg relative cursor-pointer group' style={{backgroundImage: `url(${project.bgImage})`}} key={index}>
                       
                       <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                       
@@ -29,18 +53,22 @@ const Work = ({isDarkMode}) => {
 
                             </div>
 
-                  </div>
+                  </motion.div>
 
                    ))}
 
-      </div>
+      </motion.div>
 
-      <a href='' className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10  mx-auto my-20 hover:[background-color:#fcf4ff] duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
+      <motion.a
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration: 0.5 , delay:1.1}}
+       href='' className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10  mx-auto my-20 hover:[background-color:#fcf4ff] duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
         Show More <Image src={isDarkMode? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='right-arrow' className='w-4' />
 
-      </a>
+      </motion.a>
 
-    </div>
+    </motion.div>
   )
 }
 
