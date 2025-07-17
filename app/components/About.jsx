6a5db3b189906b005999a56554ed0136f3d9e003
ShireenShamil <shireenshamil11@gmail.com>
2025-07-17 12:44:53 +0530

    updates

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -176,6 +176,7 @@ const About = ({ isDarkMode }) => {
                     </AnimatePresence>
                   </div>
                 )}
+                
             </div>
           </div>
 
