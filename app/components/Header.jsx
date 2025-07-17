ce1290812d7f2fff5d7b85db83b7de87bdc1ac5b
ShireenShamil <shireenshamil11@gmail.com>
2025-07-17 11:32:29 +0530

    CV updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -44,7 +44,7 @@ const Header = ({isDarkMode}) => {
          initial={{y:30 , opacity: 0}}
         whileInView={{y:0 , opacity:1}}
         transition={{duration: 0.6 , delay: 1.2}}
-        href='/SS.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>
+        href='/Shireen.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>
          My Resume <Image src={ assets.download_icon} alt='' className=' w-4'/>
         </motion.a>
       </div>
