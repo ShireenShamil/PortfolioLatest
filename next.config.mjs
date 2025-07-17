005e0f59671f445409ce9a8c1ecc7701f6fc90ca
ShireenShamil <shireenshamil11@gmail.com>
2025-07-17 12:02:08 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
+};
 
 export default nextConfig;
