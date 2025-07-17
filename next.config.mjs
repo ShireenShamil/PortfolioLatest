bd64cfd581a2392636ca90e1b730a03c56fe55d7
ShireenShamil <shireenshamil11@gmail.com>
2025-07-17 12:47:31 +0530

    conflict cleared

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/Portfolio',
-  assetPrefix: '/Portfolio/',
+ 
 };
 
 export default nextConfig;
